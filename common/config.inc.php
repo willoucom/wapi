57aef5af5c05aa14e3dc9de62e1b690bd9f8edf8
Wilfried JEANNIARD <willou.com@gmail.com>
2014-06-09 14:06:17 +0200

    Ajout d'un gestionnaire de routes

--- common/config.inc.php
+++ common/config.inc.php
@@ -1,4 +1,6 @@
 <?php
-
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+
+chdir(dirname(__FILE__));
+chdir('../');
--- scripts/postdeploy/postdeployscript.php
+++ scripts/postdeploy/postdeployscript.php
@@ -1,5 +1,37 @@
 <?php
+chdir(dirname(__FILE__));
+require '../../common/config.inc.php';
 /**
+ * Script de post deployement
+ */
+
+
+// je me place à la racine du projet
+system('pwd');
+
+
+// composer
+system('composer install');
 
 
 
+$routes = parse_ini_file('routes.ini',true);
+print_r($routes);
+
+foreach ($routes['latest'] as $link => $directory) {
+    $directory = "../version/".$directory;
+    $link = "latest/".$link;
+    unlink($link);
+    echo "creation de " . $link ." pointant vers " . $directory ."\n";
+    symlink($directory, $link);
+
+}
+
+foreach ($routes['beta'] as $link => $directory) {
+    $directory = "../version/".$directory;
+    $link = "beta/".$link;
+    unlink($link);
+    echo "creation de " . $link ." pointant vers " . $directory ."\n";
+    symlink($directory, $link);
+
+}
